62a26773e76f077a358a75ecb60c2691a2039d7a
Steen Larsen <steen666@gmail.com>
2012-01-26 19:20:03 +0100

    added a finder by id

--- src/dynamo4clj/core.clj
+++ src/dynamo4clj/core.clj
@@ -1,18 +1,15 @@
 (ns dynamo4clj.core
   (:import [com.amazonaws.auth AWSCredentials PropertiesCredentials]
            [com.amazonaws.services.dynamodb AmazonDynamoDBClient]
-           [com.amazonaws.services.dynamodb.model AttributeValue PutItemRequest]
+           [com.amazonaws.services.dynamodb.model AttributeValue PutItemRequest QueryRequest ]
            [com.amazonaws AmazonServiceException ClientConfiguration Protocol]
            [java.util HashMap]))
 
 (defn get-client []
   (let [creds (PropertiesCredentials. (.getResourceAsStream (clojure.lang.RT/baseLoader) "aws.properties"))
         config (ClientConfiguration.)]
-    (. config (setProxyHost "tglarray06.tdk.dk"))
-    (. config (setProxyPort 8080))
-    (. config (setProxyUsername "m00522"))
-    (. config (setProxyPassword "skrid#nar20"))
-    (. config (setProxyWorkstation "xpn55455"))
+    (comment (. config (setProxyHost "tglarray06.tdk.dk"))
+             (. config (setProxyPort 8080)))
     (. config (setProtocol Protocol/HTTP))
     (. config (setMaxErrorRetry 3))
     (AmazonDynamoDBClient. creds config)))
@@ -20,9 +17,11 @@
 (def client (get-client))
 
 (defn insert [data]
-  (let [hmap (HashMap.)
-        attrv (AttributeValue.)]
-    (doall (map #(. hmap (put (name %) (. attrv (withS (get data %))))) (keys data)))
-    (let [req (PutItemRequest.)]
-      (. req (withTableName "abonnement") (withItem hmap))
-      (. client (putItem req)))))
+  (let [hmap (HashMap.)]
+    (doall (map #(doto hmap (.put (name %) (doto (AttributeValue.) (.withS (get data %))))) (keys data)))
+    (let [req (doto (PutItemRequest.) (.withTableName "abonnement") (.withItem hmap))]      
+      (doto client (.putItem req)))))
+
+(defn find-abon [id]
+  (let [req (doto (QueryRequest.) (.withTableName "abonnement") (.withHashKeyValue (doto (AttributeValue.) (.withS id))))]
+    (map #(prn %) (.getItems (. client (query req))))))
