db8d9780b15e585b7a185ce79538a7716f70d585
Thomas Engelschmidt <thenge@yousee.dk>
2013-01-23 10:49:58 +0100

    Bumped to amazon sdk to 1.3.27, and corrected project.clj to lein 2.0 format

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject dynamo4clj "1.0.12"
+(defproject dynamo4clj "1.0.13"
   :description "Amazon DynamoDB API"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.14"]
+                 [com.amazonaws/aws-java-sdk "1.3.27"]
                  [org.clojure/algo.generic "0.1.0"]]
-  :dev-dependencies [[swank-clojure "1.4.0"]
-                    [yij/lein-plugins "1.0.5"]]
+
+  :plugins [[yij/lein-plugins "1.0.12"]
+            [swank-clojure "1.4.0"]]
   :warn-on-reflection true)
